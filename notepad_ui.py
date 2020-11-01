526b27c9c6a3e25d459d406be59abe2243d12f5f
Shadman Quazi <shadmanhquazi@gmail.com>
2020-11-01 14:17:03 -0500

    button events for ui script + canvas functionality

--- notepad_ui.py
+++ notepad_ui.py
@@ -2,20 +2,34 @@
 
 from tkinter import *
 
+def LeftClick(event):
+    print("Save function")
+
+def CanvasClick(event):
+    print("Canvas clicked")
+    canvas.create_rectangle(event.x,event.y,event.x+2,event.y+2,width=3)
+    
 window = Tk()
 # theLabel=Label(window, text="Please write something below")
 # theLabel.pack()
 # window.title("Test Window")
 
-window.geometry("960x540")
+#window.geometry("960x540")
 topFrame=Frame(window)
-topFrame.grid(row=0,column=0)
+topFrame.pack()
 
 
-# bottomFrame=Frame(window)
-# bottomFrame.pack(side=BOTTOM)
+bottomFrame=Frame(window)
+bottomFrame.pack(side=BOTTOM)
 
 button1 = Button(topFrame,text="Save written",fg="green")
-button1.grid(row=0,column=0)
+button1.bind("<Button-1>",LeftClick)
+# # button1.grid(row=0,column=0)
+button1.pack()
+
+canvas = Canvas(bottomFrame,width=960,height=540,bg="old lace")
+canvas.bind("<B1-Motion>",CanvasClick)
+canvas.pack()
+#canvas.grid(row=0,column=2)
 
 window.mainloop()
